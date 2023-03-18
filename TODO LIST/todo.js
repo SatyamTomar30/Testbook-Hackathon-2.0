1cef888e64636078563f3ca300615cf76fbc7561
SatyamTomar30 <118533103+SatyamTomar30@users.noreply.github.com>
2023-03-18 21:04:03 +0530

    Add files via upload

---
+++ TODO LIST/todo.js
@@ -0,0 +1,51 @@
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let tasks = document.querySelector('.tasks');
+
+//add btn disabled
+
+input.addEventListener('keyup',()=>{
+    if(input.value.trim() !=0){
+        addBtn.classList.add('active')
+    }else{
+        addBtn.classList.remove('active')
+    }
+})
+
+//add new item to list
+
+addBtn.addEventListener('click',()=>{
+    if(input.value.trim() !=0){
+        let newItem = document.createElement('div');
+        newItem.classList.add('Item');
+        newItem.innerHTML = `
+        <p>${input.value}</p>
+            <div class="Item-btn">
+             <i class="fa-solid fa-pen-to-square"></i>
+             <i class="fa-solid fa-square-xmark"></i>
+            </div>      
+            ` 
+            tasks.appendChild(newItem);
+            input.value = '';
+    }else{
+        alert('please enter a task')
+    }
+})
+
+// //delete item from task list
+
+tasks.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-square-xmark')){
+    e.target.parentElement.parentElement.remove();
+  }
+})
+
+// //mark item as completed
+
+tasks.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('fa-pen-to-square')
+    ) { 
+        e.target.parentElement.parentElement.classList.toggle('completed');
+    }
+
+ });
